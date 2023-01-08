44e90aff5cf27ecc74d1a68c888117db689e2069
Rohan Soni <sonirohan94@gmail.com>
2023-01-08 13:00:26 +0530

    This is my first commit

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 import pickle
 import os
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, jsonify, request, url_for, render_template
 import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
-model = pickle.load(open('gbdt_clf_i_p.pickle','rb'))
+model = pickle.load(open('gbdt_clf_i_p.pkl','rb'))
+scaler= pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/predict_api', method=['POST'])
-
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     data= request.json['data']
     print(data)
-    print(np.array(list(data.values()).reshape(1,-1)))
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data= scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output= model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data= [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output= model.predict(final_input)[0]
+    return render_template("home.html", prediction_text= "The predicted salary is {}".format(output))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
 
 
